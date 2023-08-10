d15d45b6316b6c84fe0f6696ccf6cd04394f97b0
Ibrahim Alhas <65875290+alhasacademy96@users.noreply.github.com>
2023-08-10 21:47:35 +0100

    initial work on end of episode notification

--- Assets/Scripts/ArenasParametersSideChannel.cs
+++ Assets/Scripts/ArenasParametersSideChannel.cs
@@ -4,32 +4,31 @@ using ArenasParameters;
 
 public class ArenasParametersSideChannel : SideChannel
 {
+	public ArenasParametersSideChannel()
+	{
+		ChannelId = new Guid("9c36c837-cad5-498a-b675-bc19c9370072");
+	}
 
-    public ArenasParametersSideChannel()
-    {
-        ChannelId = new Guid("9c36c837-cad5-498a-b675-bc19c9370072");
-    }
+	protected override void OnMessageReceived(IncomingMessage msg)
+	{
+		// When a new message is received we trigger an event to signal the environment
+		// configurations to check if they need to update
+		ArenasParametersEventArgs args = new ArenasParametersEventArgs();
+		args.arenas_yaml = msg.GetRawBytes();
+		OnArenasParametersReceived(args);
+	}
 
-    protected override void OnMessageReceived(IncomingMessage msg)
-    {
-        // when a new message is received we trigger an event to signal the environment
-        // configurations to check if they need to update
-        ArenasParametersEventArgs args = new ArenasParametersEventArgs();
-        args.arenas_yaml = msg.GetRawBytes();
-        OnArenasParametersReceived(args);
-    }
+	protected virtual void OnArenasParametersReceived(
+		ArenasParametersEventArgs arenasParametersEvent
+	)
+	{
+		EventHandler<ArenasParametersEventArgs> handler = NewArenasParametersReceived;
+		if (handler != null)
+		{
+			handler(this, arenasParametersEvent);
+		}
+	}
 
-    protected virtual void OnArenasParametersReceived(ArenasParametersEventArgs arenasParametersEvent)
-    {
-        EventHandler<ArenasParametersEventArgs> handler = NewArenasParametersReceived;
-        if (handler != null)
-        {
-            handler(this, arenasParametersEvent);
-        }
-    }
-
-    public EventHandler<ArenasParametersEventArgs> NewArenasParametersReceived;
-    
-    //Currently no way to get feedback for which items spawned successfully
-
+	public EventHandler<ArenasParametersEventArgs> NewArenasParametersReceived;|
+	
 }
