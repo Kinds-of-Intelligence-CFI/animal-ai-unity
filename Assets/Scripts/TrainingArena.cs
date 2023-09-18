d1cce736f2c3cfe175b56ade386891076db7e8bd
Ibrahim Alhas <65875290+alhasacademy96@users.noreply.github.com>
2023-09-18 20:12:29 +0100

    Update TrainingArena.cs
    Potential fix for the arenas not cycling properly. Current testing suggests it now cycles through all the arenas in order. The arena index starts at 1, as opposed to 0 now. Also added further error checking for robustness.

--- Assets/Scripts/TrainingArena.cs
+++ Assets/Scripts/TrainingArena.cs
@@ -9,135 +9,156 @@ using Random = UnityEngine.Random;
 
 public class TrainingArena : MonoBehaviour
 {
-	public ListOfPrefabs prefabs = new ListOfPrefabs();
-	public GameObject spawnedObjectsHolder;
-	public int maxSpawnAttemptsForAgent = 100;
-	public int maxSpawnAttemptsForPrefabs = 20;
-	public ListOfBlackScreens blackScreens = new ListOfBlackScreens();
+    public ListOfPrefabs prefabs = new ListOfPrefabs();
+    public GameObject spawnedObjectsHolder;
+    public int maxSpawnAttemptsForAgent = 100;
+    public int maxSpawnAttemptsForPrefabs = 20;
+    public ListOfBlackScreens blackScreens = new ListOfBlackScreens();
 
-	[HideInInspector]
-	public int arenaID = -1;
+    [HideInInspector]
+    public int arenaID = 1;
 
-	[HideInInspector]
-	public int maxarenaID = -1;
+    [HideInInspector]
+    public int maxarenaID = -1;
 
-	[HideInInspector]
-	public TrainingAgent _agent;
-	private ArenaBuilder _builder;
-	public ArenaBuilder Builder
-	{
-		get { return _builder; }
-	}
-	private ArenaConfiguration _arenaConfiguration = new ArenaConfiguration();
-	private AAI3EnvironmentManager _environmentManager;
-	private List<Fade> _fades = new List<Fade>();
-	private bool _lightStatus = true;
-	private int _agentDecisionInterval; // How many frames between decisions, reads from agent's decision requester
-	private bool _firstReset = true;
-	private List<GameObject> spawnedRewards = new List<GameObject>();
-	public bool showNotification { get; set; }
+    [HideInInspector]
+    public TrainingAgent _agent;
+    private ArenaBuilder _builder;
+    public ArenaBuilder Builder
+    {
+        get { return _builder; }
+    }
+    private ArenaConfiguration _arenaConfiguration = new ArenaConfiguration();
+    private AAI3EnvironmentManager _environmentManager;
+    private List<Fade> _fades = new List<Fade>();
+    private bool _lightStatus = true;
+    private int _agentDecisionInterval; // How many frames between decisions, reads from agent's decision requester
+    private bool _firstReset = true;
+    private List<GameObject> spawnedRewards = new List<GameObject>();
+    public bool showNotification { get; set; }
 
-	internal void Awake()
-	{
-		_builder = new ArenaBuilder(
-			gameObject,
-			spawnedObjectsHolder,
-			maxSpawnAttemptsForPrefabs,
-			maxSpawnAttemptsForAgent
-		);
-		_environmentManager = GameObject.FindObjectOfType<AAI3EnvironmentManager>();
-		_agent = FindObjectsOfType<TrainingAgent>(true)[0];
-		_agentDecisionInterval = _agent.GetComponentInChildren<DecisionRequester>().DecisionPeriod;
-		_fades = blackScreens.GetFades();
+    internal void Awake()
+    {
+        _builder = new ArenaBuilder(
+            gameObject,
+            spawnedObjectsHolder,
+            maxSpawnAttemptsForPrefabs,
+            maxSpawnAttemptsForAgent
+        );
+        _environmentManager = GameObject.FindObjectOfType<AAI3EnvironmentManager>();
+        _agent = FindObjectsOfType<TrainingAgent>(true)[0];
+        _agentDecisionInterval = _agent.GetComponentInChildren<DecisionRequester>().DecisionPeriod;
+        _fades = blackScreens.GetFades();
 
-		// Subscribe to the reward spawn event from spawner_InteractiveButton.cs
-		Spawner_InteractiveButton.RewardSpawned += OnRewardSpawned;
-	}
+        // Subscribe to the reward spawn event from spawner_InteractiveButton.cs
+        Spawner_InteractiveButton.RewardSpawned += OnRewardSpawned;
+    }
 
-	private void OnDestroy()
-	{
-		// Unsubscribe when the object is destroyed to avoid memory leaks
-		Spawner_InteractiveButton.RewardSpawned -= OnRewardSpawned;
-	}
+    private void OnDestroy()
+    {
+        // Unsubscribe when the object is destroyed to avoid memory leaks
+        Spawner_InteractiveButton.RewardSpawned -= OnRewardSpawned;
+    }
 
-	private void OnRewardSpawned(GameObject reward)
-	{
-		spawnedRewards.Add(reward);
-	}
+    private void OnRewardSpawned(GameObject reward)
+    {
+        spawnedRewards.Add(reward);
+    }
 
-	public void ResetArena()
-	{
-		Debug.Log("Resetting Arena");
-		foreach (GameObject holder in transform.FindChildrenWithTag("spawnedObjects"))
-		{
-			holder.SetActive(false);
-			Destroy(holder);
-		}
+    public void ResetArena()
+    {
+        Debug.Log("Resetting Arena");
+        foreach (GameObject holder in transform.FindChildrenWithTag("spawnedObjects"))
+        {
+            holder.SetActive(false);
+            Destroy(holder);
+        }
 
-		// Each time reset is called we cycle through the defined arenaIDs
-		maxarenaID = _environmentManager.getMaxArenaID(); // Should perform this check once (after environmentManager is initialized).
-		if (maxarenaID > 0 && !_firstReset)
-		{
-			arenaID = (arenaID + 1) % maxarenaID;
-		}
+        maxarenaID = _environmentManager.getMaxArenaID();
+        if (_firstReset)
+        {
+            arenaID = 1; // Start with the first arena on the very first reset. Fix for the first arena not being loaded.
+            _firstReset = false;
+        }
+        else
+        {
+            arenaID = (arenaID + 1) % (maxarenaID + 1); // Increment the arenaID for subsequent resets.
+        }
 
-		ArenaConfiguration newConfiguration;
-		if (!_environmentManager.GetConfiguration(arenaID, out newConfiguration))
-		{
-			newConfiguration = new ArenaConfiguration(prefabs);
-			_environmentManager.AddConfiguration(arenaID, newConfiguration);
-		}
-		_arenaConfiguration = newConfiguration;
+        ArenaConfiguration newConfiguration;
 
-		// Pass the showNotification attribute to the agent
-		_agent.showNotification = _arenaConfiguration.showNotification;
+        int attempts = 0; // To prevent infinite loops in case of a major error.
 
-		Debug.Log("Updating");
-		_arenaConfiguration.SetGameObject(prefabs.GetList());
-		_builder.Spawnables = _arenaConfiguration.spawnables;
-		_arenaConfiguration.toUpdate = false;
-		_agent.MaxStep = 0; // We never time the environment out unless agent health goes to 0
-		_agent.timeLimit = _arenaConfiguration.T * _agentDecisionInterval;
-		_builder.Build();
-		_arenaConfiguration.lightsSwitch.Reset();
-		_firstReset = false;
+        while (
+            !_environmentManager.GetConfiguration(arenaID, out newConfiguration)
+            && attempts <= maxarenaID
+        )
+        {
+            Debug.LogWarning(
+                "No configuration found for arenaID: " + arenaID + ". Trying next arena."
+            );
+            arenaID = (arenaID + 1) % (maxarenaID + 1);
+            attempts++;
+        }
 
-		if (_arenaConfiguration.randomSeed != 0)
-		{
-			Random.InitState(_arenaConfiguration.randomSeed);
-		}
+        if (attempts == maxarenaID + 1)
+        {
+            Debug.LogError(
+                "Unable to find a matching configuration for any arena. Check your config setup."
+            );
+            return; // Early exit from the function.
+        }
 
-		foreach (var reward in spawnedRewards)
-		{
-			Destroy(reward);
-		}
-		spawnedRewards.Clear();
-	}
+        _arenaConfiguration = newConfiguration;
 
-	public void UpdateLigthStatus()
-	{
-		int stepCount = _agent.StepCount;
-		bool newLight = _arenaConfiguration.lightsSwitch.LightStatus(
-			stepCount,
-			_agentDecisionInterval
-		);
-		if (newLight != _lightStatus)
-		{
-			_lightStatus = newLight;
-			foreach (Fade fade in _fades)
-			{
-				fade.StartFade();
-			}
-		}
-	}
+        // Pass the showNotification attribute to the agent.
+        _agent.showNotification = _arenaConfiguration.showNotification;
 
-	void FixedUpdate()
-	{
-		UpdateLigthStatus();
-	}
+        Debug.Log("Updating");
+        _arenaConfiguration.SetGameObject(prefabs.GetList());
+        _builder.Spawnables = _arenaConfiguration.spawnables;
+        _arenaConfiguration.toUpdate = false;
+        _agent.MaxStep = 0; // We never time the environment out unless agent health goes to 0.
+        _agent.timeLimit = _arenaConfiguration.T * _agentDecisionInterval;
+        _builder.Build();
+        _arenaConfiguration.lightsSwitch.Reset();
 
-	public ArenaConfiguration ArenaConfig
-	{
-		get { return _arenaConfiguration; }
-	}
+        if (_arenaConfiguration.randomSeed != 0)
+        {
+            Random.InitState(_arenaConfiguration.randomSeed);
+        }
+
+        foreach (var reward in spawnedRewards)
+        {
+            Destroy(reward);
+        }
+        spawnedRewards.Clear();
+    }
+
+    public void UpdateLigthStatus()
+    {
+        int stepCount = _agent.StepCount;
+        bool newLight = _arenaConfiguration.lightsSwitch.LightStatus(
+            stepCount,
+            _agentDecisionInterval
+        );
+        if (newLight != _lightStatus)
+        {
+            _lightStatus = newLight;
+            foreach (Fade fade in _fades)
+            {
+                fade.StartFade();
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        UpdateLigthStatus();
+    }
+
+    public ArenaConfiguration ArenaConfig
+    {
+        get { return _arenaConfiguration; }
+    }
 }
