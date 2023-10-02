9ec7b90dc1e8f70c7b3ff81606e1cc93ab35d46c
ibrahim alhas <alhasacademy@gmail.com>
2023-10-02 23:19:36 +0100

    Revert "pillar button can now be set at any y axis <= 100"
    This reverts commit 1f760222914efa31c90c5efd652604e451ed0f1b.

--- Assets/Prefabs/Spawners/Spawner_InteractiveButton.cs
+++ Assets/Prefabs/Spawners/Spawner_InteractiveButton.cs
@@ -6,309 +6,278 @@ using ArenaBuilders;
 
 public class Spawner_InteractiveButton : MonoBehaviour
 {
-    private bool IsMoving = false;
-    private float lastInteractionTime;
-    private float totalInteractionInterval = 0f;
-    public int ButtonPressCount { get; private set; }
-    public GameObject LastSpawnedReward { get; private set; }
-    public Dictionary<GameObject, int> RewardSpawnCounts { get; private set; } =
-        new Dictionary<GameObject, int>();
+	private bool IsMoving = false;
+	private float lastInteractionTime;
+	private float totalInteractionInterval = 0f;
+	public int ButtonPressCount { get; private set; }
+	public GameObject LastSpawnedReward { get; private set; }
+	public Dictionary<GameObject, int> RewardSpawnCounts { get; private set; } = new Dictionary<GameObject, int>();
+	[SerializeField] private GameObject childObjectToMove;
+	[SerializeField] private Vector3 moveOffset;
+	[SerializeField] private Transform rewardSpawnPoint;
+	[SerializeField] private GameObject objectToControl;
+	[SerializeField] private bool randomizeColor = false;
+	[SerializeField] private bool showObject;
+	[SerializeField] private ArenaBuilder arenaBuilder;
+	private Transform objectToControlSpawnPoint;
+	private List<GameObject> rewards;
+	private List<float> rewardWeights;
+	public delegate void OnRewardSpawned(GameObject reward);
+	public static event OnRewardSpawned RewardSpawned;
+	private float _moveDuration;
+	private float _resetDuration;
+	public float SpawnProbability { get; set; } = 1f;
+	public float MoveDuration
+	{
+		get { return _moveDuration; }
+		set { _moveDuration = value; }
+	}
+	public float ResetDuration
+	{
+		get { return _resetDuration; }
+		set { _resetDuration = value; }
+	}
+	public List<string> RewardNames { get; set; }
+	public List<float> RewardWeights { get; set; }
+	public List<GameObject> Rewards { get; set; }
+	public Vector3 RewardSpawnPos { get; set; }
+	public List<int> MaxRewardCounts { get; set; }
 
-    [SerializeField]
-    private GameObject childObjectToMove;
+	void Start()
+	{
+		lastInteractionTime = Time.time;
+		UpdateObjectVisibility(); // Set the object to be visible or not based on the showObject flag
 
-    [SerializeField]
-    private Vector3 moveOffset;
+		if (RewardNames != null && RewardNames.Count > 0)
+		{
+			Rewards = RewardNames.Select(name =>
+			{
+				GameObject reward = Resources.Load<GameObject>(name);
+				if (reward == null)
+				{
+					Debug.LogError($"Failed to load reward: {name}");
+				}
+				return reward;
+			}).ToList();
+		}
 
-    [SerializeField]
-    private Transform rewardSpawnPoint;
+		rewardWeights = RewardWeights;
 
-    [SerializeField]
-    private GameObject objectToControl;
+		if (randomizeColor)
+		{
+			RandomizeColor();
+		}
+	}
 
-    [SerializeField]
-    private bool randomizeColor = false;
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("agent"))
+		{
+			if (IsMoving)
+			{
+				return;
+			}
 
-    [SerializeField]
-    private bool showObject;
+			ButtonPressCount++;
+			StartCoroutine(MoveAndReset());
+		}
+	}
 
-    [SerializeField]
-    private ArenaBuilder arenaBuilder;
-    private Transform objectToControlSpawnPoint;
-    private List<GameObject> rewards;
-    private List<float> rewardWeights;
-    public delegate void OnRewardSpawned(GameObject reward);
-    public static event OnRewardSpawned RewardSpawned;
-    private float _moveDuration;
-    private float _resetDuration;
-    public float SpawnProbability { get; set; } = 1f;
-    public float MoveDuration
-    {
-        get { return _moveDuration; }
-        set { _moveDuration = value; }
-    }
-    public float ResetDuration
-    {
-        get { return _resetDuration; }
-        set { _resetDuration = value; }
-    }
-    public List<string> RewardNames { get; set; }
-    public List<float> RewardWeights { get; set; }
-    public List<GameObject> Rewards { get; set; }
-    public Vector3 RewardSpawnPos { get; set; }
-    public List<int> MaxRewardCounts { get; set; }
+	private void UpdateObjectVisibility()
+	{
+		if (objectToControl != null)
+		{
+			objectToControl.SetActive(showObject);
+		}
+	}
 
-    void Start()
-    {
-        lastInteractionTime = Time.time;
-        UpdateObjectVisibility(); // Set the object to be visible or not based on the showObject flag
+	public bool MoveToTarget(Vector3 origin, Vector3 target, float startTime, float duration)
+	{
+		float t = (Time.time - startTime) / duration;
+		childObjectToMove.transform.position = Vector3.Lerp(origin, target, t);
+		return Time.time < startTime + duration;
+	}
 
-        if (RewardNames != null && RewardNames.Count > 0)
-        {
-            Rewards = RewardNames
-                .Select(name =>
-                {
-                    GameObject reward = Resources.Load<GameObject>(name);
-                    if (reward == null)
-                    {
-                        Debug.LogError($"Failed to load reward: {name}");
-                    }
-                    return reward;
-                })
-                .ToList();
-        }
+	public IEnumerator MoveAndReset()
+	{
+		IsMoving = true;
 
-        rewardWeights = RewardWeights;
+		Vector3 originalPosition = childObjectToMove.transform.position;
+		Vector3 targetPosition = originalPosition + moveOffset;
+		float startTime = Time.time;
 
-        if (randomizeColor)
-        {
-            RandomizeColor();
-        }
-    }
+		while (MoveToTarget(originalPosition, targetPosition, startTime, MoveDuration))
+		{
+			yield return null;
+		}
+		childObjectToMove.transform.position = targetPosition;
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("agent"))
-        {
-            if (IsMoving)
-            {
-                return;
-            }
+		startTime = Time.time;
+		while (MoveToTarget(targetPosition, originalPosition, startTime, ResetDuration))
+		{
+			yield return null;
+		}
+		childObjectToMove.transform.position = originalPosition;
 
-            ButtonPressCount++;
-            StartCoroutine(MoveAndReset());
-        }
-    }
+		SpawnReward();
 
-    private void UpdateObjectVisibility()
-    {
-        if (objectToControl != null)
-        {
-            objectToControl.SetActive(showObject);
-        }
-    }
+		IsMoving = false;
+	}
 
-    public bool MoveToTarget(Vector3 origin, Vector3 target, float startTime, float duration)
-    {
-        float t = (Time.time - startTime) / duration;
-        childObjectToMove.transform.position = Vector3.Lerp(origin, target, t);
-        return Time.time < startTime + duration;
-    }
+	private GameObject ChooseReward()
+	{
+		if (Rewards == null || rewardWeights == null || Rewards.Count != rewardWeights.Count)
+		{
+			Debug.LogError("Invalid rewards or reward weights setup.");
+			return null;
+		}
 
-    public IEnumerator MoveAndReset()
-    {
-        IsMoving = true;
+		float totalWeight = rewardWeights.Sum();
+		float randomNumber = Random.Range(0, totalWeight - float.Epsilon);
+		float cumulativeWeight = 0;
 
-        Vector3 originalPosition = childObjectToMove.transform.position;
-        Vector3 targetPosition = originalPosition + moveOffset;
-        float startTime = Time.time;
+		for (int i = 0; i < Rewards.Count; i++)
+		{
+			cumulativeWeight += rewardWeights[i];
+			if (randomNumber <= cumulativeWeight)
+			{
+				return Rewards[i];
+			}
+		}
 
-        while (MoveToTarget(originalPosition, targetPosition, startTime, MoveDuration))
-        {
-            yield return null;
-        }
-        childObjectToMove.transform.position = targetPosition;
+		// If no reward is selected within the loop (which should not happen), return the last reward
+		return Rewards[Rewards.Count - 1];
+	}
 
-        startTime = Time.time;
-        while (MoveToTarget(targetPosition, originalPosition, startTime, ResetDuration))
-        {
-            yield return null;
-        }
-        childObjectToMove.transform.position = originalPosition;
+	private void SpawnReward()
+	{
+		GameObject rewardToSpawn = ChooseReward();
 
-        SpawnReward();
+		if (rewardToSpawn == null)
+		{
+			Debug.LogError("Failed to choose a reward to spawn.");
+			return;
+		}
 
-        IsMoving = false;
-    }
+		int rewardIndex = Rewards.IndexOf(rewardToSpawn);
+		if (rewardIndex == -1)
+		{
+			Debug.LogError("Chosen reward is not in the Rewards list.");
+			return;
+		}
 
-    private GameObject ChooseReward()
-    {
-        if (Rewards == null || rewardWeights == null || Rewards.Count != rewardWeights.Count)
-        {
-            Debug.LogError("Invalid rewards or reward weights setup.");
-            return null;
-        }
+		if (rewardIndex < MaxRewardCounts.Count)
+		{
+			Debug.Log("Max allowed spawns for " + rewardToSpawn.name + ": " + MaxRewardCounts[rewardIndex]);
+		}
+		else
+		{
+			Debug.Log("No max spawn count set for " + rewardToSpawn.name);
+		}
 
-        float totalWeight = rewardWeights.Sum();
-        float randomNumber = Random.Range(0, totalWeight - float.Epsilon);
-        float cumulativeWeight = 0;
+		if (MaxRewardCounts != null && rewardIndex < MaxRewardCounts.Count && MaxRewardCounts[rewardIndex] != -1)
+		{
+			if (RewardSpawnCounts.TryGetValue(rewardToSpawn, out var count) && count >= MaxRewardCounts[rewardIndex])
+			{
+				Debug.Log("Max reward count reached for reward: " + rewardToSpawn.name);
+				return;
+			}
+		}
 
-        for (int i = 0; i < Rewards.Count; i++)
-        {
-            cumulativeWeight += rewardWeights[i];
-            if (randomNumber <= cumulativeWeight)
-            {
-                return Rewards[i];
-            }
-        }
+		if (Rewards == null || Rewards.Count == 0)
+		{
+			Debug.LogError("No rewards are set to be spawned.");
+			return;
+		}
 
-        // If no reward is selected within the loop (which should not happen), return the last reward
-        return Rewards[Rewards.Count - 1];
-    }
+		if (Random.value <= SpawnProbability)
+		{
+			Vector3 spawnPosition = rewardSpawnPoint.position;
 
-    private void SpawnReward()
-    {
-        GameObject rewardToSpawn = ChooseReward();
+			if (RewardSpawnPos != Vector3.zero)
+			{
+				spawnPosition = RewardSpawnPos;
+			}
+			// Otherwise, random spawning.
+			else
+			{
+				float arenaWidth = arenaBuilder.GetArenaWidth();
+				float arenaDepth = arenaBuilder.GetArenaDepth();
 
-        if (rewardToSpawn == null)
-        {
-            Debug.LogError("Failed to choose a reward to spawn.");
-            return;
-        }
+				// Randomly generate a spawn position within the bounds of the arena, as defined by Arenabuilders.cs.
+				spawnPosition = new Vector3(
+					Random.Range(0, arenaWidth),
+					0,  // Assuming spawning on the ground.
+					Random.Range(0, arenaDepth)
+				);
+			}
+			// Check for randomization flags for x and z axes
+			if (RewardSpawnPos.x == -1)
+			{
+				spawnPosition.x = Random.Range(0, arenaBuilder.GetArenaWidth());
+			}
 
-        int rewardIndex = Rewards.IndexOf(rewardToSpawn);
-        if (rewardIndex == -1)
-        {
-            Debug.LogError("Chosen reward is not in the Rewards list.");
-            return;
-        }
+			if (RewardSpawnPos.y == -1)
+			{
+				spawnPosition.y = Random.Range(0, 100);
+				Debug.Log("Randomized y: " + spawnPosition.y);  // Random value between 0 and 100 for the y-axis to make sure no object is too high.
+			}
+			else
+			{
+				spawnPosition.y = RewardSpawnPos.y;
+				Debug.Log("Set y to: " + spawnPosition.y);
+			}
 
-        if (rewardIndex < MaxRewardCounts.Count)
-        {
-            Debug.Log(
-                "Max allowed spawns for " + rewardToSpawn.name + ": " + MaxRewardCounts[rewardIndex]
-            );
-        }
-        else
-        {
-            Debug.Log("No max spawn count set for " + rewardToSpawn.name);
-        }
+			if (RewardSpawnPos.z == -1)
+			{
+				spawnPosition.z = Random.Range(0, arenaBuilder.GetArenaDepth());
+			}
 
-        if (
-            MaxRewardCounts != null
-            && rewardIndex < MaxRewardCounts.Count
-            && MaxRewardCounts[rewardIndex] != -1
-        )
-        {
-            if (
-                RewardSpawnCounts.TryGetValue(rewardToSpawn, out var count)
-                && count >= MaxRewardCounts[rewardIndex]
-            )
-            {
-                Debug.Log("Max reward count reached for reward: " + rewardToSpawn.name);
-                return;
-            }
-        }
+			LastSpawnedReward = Instantiate(rewardToSpawn, spawnPosition, Quaternion.identity);
 
-        if (Rewards == null || Rewards.Count == 0)
-        {
-            Debug.LogError("No rewards are set to be spawned.");
-            return;
-        }
+			if (RewardSpawnCounts.TryGetValue(rewardToSpawn, out var count))
+			{
+				RewardSpawnCounts[rewardToSpawn] = count + 1;
+			}
+			else
+			{
+				RewardSpawnCounts[rewardToSpawn] = 1;
+			}
 
-        if (Random.value <= SpawnProbability)
-        {
-            Vector3 spawnPosition = rewardSpawnPoint.position;
+			if (showObject && objectToControl != null)
+			{
+				Instantiate(objectToControl, objectToControlSpawnPoint.transform.position, Quaternion.identity);
+			}
 
-            if (RewardSpawnPos != Vector3.zero)
-            {
-                spawnPosition = RewardSpawnPos;
-            }
-            // Otherwise, random spawning.
-            else
-            {
-                float arenaWidth = arenaBuilder.GetArenaWidth();
-                float arenaDepth = arenaBuilder.GetArenaDepth();
+			float currentInteractionTime = Time.time;
+			totalInteractionInterval += currentInteractionTime - lastInteractionTime;
+			lastInteractionTime = currentInteractionTime;
 
-                // Randomly generate a spawn position within the bounds of the arena, as defined by Arenabuilders.cs.
-                spawnPosition = new Vector3(
-                    Random.Range(0, arenaWidth),
-                    0, // Assuming spawning on the ground.
-                    Random.Range(0, arenaDepth)
-                );
-            }
-            // Check for randomization flags for x and z axes
-            if (RewardSpawnPos.x == -1)
-            {
-                spawnPosition.x = Random.Range(0, arenaBuilder.GetArenaWidth());
-            }
+			RewardSpawned?.Invoke(LastSpawnedReward);
+		}
+	}
 
-            if (RewardSpawnPos.y == -1)
-            {
-                spawnPosition.y = Random.Range(0, 100);
-                Debug.Log("Randomized y: " + spawnPosition.y); // Random value between 0 and 100 for the y-axis to make sure no object is too high.
-            }
-            else
-            {
-                spawnPosition.y = RewardSpawnPos.y;
-                Debug.Log("Set y to: " + spawnPosition.y);
-            }
+	public float GetAverageInteractionInterval()
+	{
+		if (ButtonPressCount == 0)
+			return 0;
 
-            if (RewardSpawnPos.z == -1)
-            {
-                spawnPosition.z = Random.Range(0, arenaBuilder.GetArenaDepth());
-            }
+		return totalInteractionInterval / ButtonPressCount;
+	}
 
-            LastSpawnedReward = Instantiate(rewardToSpawn, spawnPosition, Quaternion.identity);
+	private void RandomizeColor()
+	{
+		Color randomColor = new Color(Random.value, Random.value, Random.value, 1.0f);
+		Renderer rend = this.gameObject.GetComponent<Renderer>();
 
-            if (RewardSpawnCounts.TryGetValue(rewardToSpawn, out var count))
-            {
-                RewardSpawnCounts[rewardToSpawn] = count + 1;
-            }
-            else
-            {
-                RewardSpawnCounts[rewardToSpawn] = 1;
-            }
+		if (rend != null)
+		{
+			rend.material.color = randomColor;
+		}
+		else
+		{
+			Debug.LogWarning("No Renderer found on the Pillar-Button to set the color. Please add a Renderer to the Pillar-Button prefab.");
+		}
+	}
 
-            if (showObject && objectToControl != null)
-            {
-                Instantiate(
-                    objectToControl,
-                    objectToControlSpawnPoint.transform.position,
-                    Quaternion.identity
-                );
-            }
-
-            float currentInteractionTime = Time.time;
-            totalInteractionInterval += currentInteractionTime - lastInteractionTime;
-            lastInteractionTime = currentInteractionTime;
-
-            RewardSpawned?.Invoke(LastSpawnedReward);
-        }
-    }
-
-    public float GetAverageInteractionInterval()
-    {
-        if (ButtonPressCount == 0)
-            return 0;
-
-        return totalInteractionInterval / ButtonPressCount;
-    }
-
-    private void RandomizeColor()
-    {
-        Color randomColor = new Color(Random.value, Random.value, Random.value, 1.0f);
-        Renderer rend = this.gameObject.GetComponent<Renderer>();
-
-        if (rend != null)
-        {
-            rend.material.color = randomColor;
-        }
-        else
-        {
-            Debug.LogWarning(
-                "No Renderer found on the Pillar-Button to set the color. Please add a Renderer to the Pillar-Button prefab."
-            );
-        }
-    }
 }
