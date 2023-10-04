c1c9ce359ad272b539f38889e1fadba88fc32728
Ibrahim Alhas <65875290+alhasacademy96@users.noreply.github.com>
2023-10-04 17:05:35 +0100

    Update PlayerControls.cs
    updated playercontrols to add layers of checks to make sure the cameras are indeed fetched properly. Also added a new button for quick action: F10 will not close the build properly without leaving traces of memory dependency.

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -16,6 +16,7 @@ public class PlayerControls : MonoBehaviour
 	private Dictionary<int, Camera> _cameras;
 	public float prevScore = 0;
 	public Canvas effectCanvas;
+	private bool camerasFetched = false;
 
 	public int cameraID
 	{
@@ -28,36 +29,12 @@ public class PlayerControls : MonoBehaviour
 
 	void Start()
 	{
-		_agent = GameObject.FindGameObjectWithTag("agent").GetComponent<TrainingAgent>();
+		effectCanvas.renderMode = RenderMode.ScreenSpaceCamera;
+		arenasConfigurations = ArenasConfigurations.Instance;
+
 		_screenshotCam = GameObject.FindGameObjectWithTag("ScreenshotCam").GetComponent<ScreenshotCamera>();
 		_cameraFollow = GameObject.FindGameObjectWithTag("camBase").GetComponent<Camera>();
 		_cameraAbove = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
-		_cameraAgent = _agent.transform.Find("AgentCamMid").GetComponent<Camera>();
-
-		if (_cameraAgent != null)
-		{
-			Debug.Log("Camera Agent fetched successfully.");
-		}
-		else
-		{
-			Debug.Log("Failed to fetch Camera Agent.");
-			return;
-		}
-
-		_cameraAbove.enabled = true;
-		_cameraAgent.enabled = false;
-		_cameraFollow.enabled = false;
-
-		_cameras = new Dictionary<int, Camera>();
-		_cameras.Add(0, _cameraAbove);
-		_cameras.Add(1, _cameraAgent);
-		_cameras.Add(2, _cameraFollow);
-
-		effectCanvas.renderMode = RenderMode.ScreenSpaceCamera;
-		effectCanvas.worldCamera = getActiveCam();
-		effectCanvas.planeDistance = choosePlaneDistance();
-
-		arenasConfigurations = ArenasConfigurations.Instance;
 
 		ArenaConfiguration currentConfig = arenasConfigurations.CurrentArenaConfiguration;
 		if (currentConfig == null)
@@ -68,21 +45,48 @@ public class PlayerControls : MonoBehaviour
 
 		_canResetEpisode = currentConfig.canResetEpisode;
 		_canChangePerspective = currentConfig.canChangePerspective;
-
-		if (!_canChangePerspective)
-		{
-			_cameraAbove.enabled = true;
-			_cameraAgent.enabled = true;
-			_cameraFollow.enabled = true;
-			_numActive = 2;
-
-			effectCanvas.worldCamera = getActiveCam();
-			effectCanvas.planeDistance = choosePlaneDistance();
-		}
 	}
 
 	void Update()
 	{
+		if (!camerasFetched)
+		{
+			_agent = GameObject.FindGameObjectWithTag("agent").GetComponent<TrainingAgent>();
+
+			if (_agent != null)
+			{
+				_cameraAgent = _agent.transform.Find("AgentCamMid").GetComponent<Camera>();
+
+				if (_cameraAgent != null)
+				{
+					camerasFetched = true;
+
+					_cameraAbove.enabled = true;
+					_cameraAgent.enabled = false;
+					_cameraFollow.enabled = false;
+
+					_cameras = new Dictionary<int, Camera>();
+					_cameras.Add(0, _cameraAbove);
+					_cameras.Add(1, _cameraAgent);
+					_cameras.Add(2, _cameraFollow);
+
+					effectCanvas.worldCamera = getActiveCam();
+					effectCanvas.planeDistance = choosePlaneDistance();
+
+					if (!_canChangePerspective)
+					{
+						_cameraAbove.enabled = false;
+						_cameraAgent.enabled = true;
+						_cameraFollow.enabled = false;
+						_numActive = 1;
+
+						effectCanvas.worldCamera = getActiveCam();
+						effectCanvas.planeDistance = choosePlaneDistance();
+					}
+				}
+			}
+		}
+
 		if (_canChangePerspective && Input.GetKeyDown(KeyCode.C))
 		{
 			UpdateCam();
@@ -98,6 +102,10 @@ public class PlayerControls : MonoBehaviour
 		if (Input.GetKeyDown(KeyCode.F9))
 		{
 			_screenshotCam.Activate();
+		}
+		if (Input.GetKeyDown(KeyCode.F10))
+		{
+			Application.Quit(); // Only works on game build, not on editor.
 		}
 
 		score.text = "Prev reward: " + _agent.GetPreviousScore().ToString("0.000")
