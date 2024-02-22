1c51e119165d385942afffec05524ae9c32f3a36
ibrahim alhas <alhasacademy@gmail.com>
2024-02-22 13:28:59 +0000

    ading mlagents references to test assembly

--- Assets/Tests/AAIEnvironmentManager.cs
+++ Assets/Tests/AAIEnvironmentManager.cs
@@ -3,7 +3,16 @@ using UnityEngine;
 using Unity.MLAgents.Sensors;
 using System.Collections.Generic;
 using System;
-using MLAgents;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using Unity.MLAgents.SideChannels;
+using Unity.MLAgents.Policies;
+
+
 
 namespace Tests
 {
