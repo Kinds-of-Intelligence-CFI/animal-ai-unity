ebb7afb2bf41da02594c8fbe129687a7c7c9c2f6
ibrahim alhas <alhasacademy@gmail.com>
2024-03-08 14:37:24 +0000

    cleaned out UIManager.cs for readabilty

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -5,25 +5,22 @@ using System.IO;
 
 public class UIManager : MonoBehaviour
 {
-    public TMP_Text arenaText; // Reference for displaying the current arena
-    public TMP_Text configFileNameText; // Reference for displaying the YAML file name
-    public AAI3EnvironmentManager environmentManager; // Reference to your environment manager component
+    public TMP_Text arenaText;
+    public TMP_Text configFileNameText;
+    public AAI3EnvironmentManager environmentManager;
 
     void Awake()
     {
-        // Subscribe to the event that notifies when the current arena changes
         AAI3EnvironmentManager.OnArenaChanged += UpdateArenaUI;
     }
 
     void OnDestroy()
     {
-        // Unsubscribe from the event to clean up
         AAI3EnvironmentManager.OnArenaChanged -= UpdateArenaUI;
     }
 
     private void Start()
     {
-        // Initial UI update to display current arena and YAML file name
         UpdateArenaUI(
             environmentManager.GetCurrentArenaIndex(),
             environmentManager.GetTotalArenas()
