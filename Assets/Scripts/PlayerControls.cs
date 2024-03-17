690f901197c2aa09c0d6a60c7672abe1b9de3018
alhasacademy96 <65875290+alhasacademy96@users.noreply.github.com>
2024-03-17 01:15:56 +0000

    cleaned up and optimized playercontrols.cs

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -2,165 +2,142 @@ using Unity.MLAgents;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using ArenasParameters;
 using TMPro;
+using ArenasParameters;
 
+/// <summary>
+/// Manages the player controls and camera perspectives.
+/// </summary>
 public class PlayerControls : MonoBehaviour
 {
-    private Camera _cameraAbove;
-    private Camera _cameraAgent;
-    private Camera _cameraFollow;
-    private ScreenshotCamera _screenshotCam;
-    private TrainingAgent _agent;
-    public TMP_Text score;
-    private int _numActive = 0;
-    private Dictionary<int, Camera> _cameras;
-    public float prevScore = 0;
-    public Canvas effectCanvas;
-    private bool camerasFetched = false;
+	[Header("Camera Settings")]
+	private Camera[] cameras = new Camera[3];
+	public int cameraID => activeCameraIndex;
+	private int activeCameraIndex = 0;
+	private bool camerasInitialized = false;
 
-    public int cameraID
-    {
-        get { return _numActive; }
-    }
+	[Header("UI Components")]
+	public TMP_Text scoreText;
+	public Canvas effectCanvas;
 
-    private bool _canResetEpisode = true;
-    private bool _canChangePerspective = true;
-    private ArenasConfigurations arenasConfigurations;
-    public UIManager uiManager;
+	[Header("Gameplay Settings")]
+	private bool canResetEpisode = true;
+	private bool canChangePerspective = true;
 
-    void Start()
-    {
-        effectCanvas.renderMode = RenderMode.ScreenSpaceCamera;
-        arenasConfigurations = ArenasConfigurations.Instance;
+	[Header("External References")]
+	private ScreenshotCamera screenshotCam;
+	private TrainingAgent agent;
+	private ArenasConfigurations arenasConfigurations;
+	public UIManager uiManager;
 
-        _screenshotCam = GameObject
-            .FindGameObjectWithTag("ScreenshotCam")
-            .GetComponent<ScreenshotCamera>();
-        _cameraFollow = GameObject.FindGameObjectWithTag("camBase").GetComponent<Camera>();
-        _cameraAbove = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+	[Header("Previous Score")]
+	public float prevScore = 0;
 
-        ArenaConfiguration currentConfig = arenasConfigurations.CurrentArenaConfiguration;
-        if (currentConfig == null)
-        {
-            Debug.LogError("Current Arena Configuration is null.");
-            return;
-        }
+	public Camera getActiveCam()
+	{
+		return cameras[activeCameraIndex];
+	}
 
-        _canResetEpisode = arenasConfigurations.canResetEpisode;
-        _canChangePerspective = arenasConfigurations.canChangePerspective;
+	private void Start()
+	{
+		InitializeCanvas();
+		LoadCamerasAndAgent();
+		LoadConfigurationSettings();
+		InitializeCameras();
+	}
 
-        // Camera Component Checks. Important check in general.
-        if (_screenshotCam == null)
-        {
-            Debug.LogError("Screenshot camera not found!");
-        }
-        if (_cameraFollow == null)
-        {
-            Debug.LogError("Camera with 'camBase' tag not found!");
-        }
-        if (_cameraAbove == null)
-        {
-            Debug.LogError("Main camera not found!");
-        }
-    }
+	private void Update()
+	{
+		if (!camerasInitialized)
+		{
+			Debug.LogError("Cameras not initialized properly.");
+			return;
+		}
 
-    void Update()
-    {
-        if (!camerasFetched)
-        {
-            _agent = GameObject.FindGameObjectWithTag("agent").GetComponent<TrainingAgent>();
+		HandleInput();
+		UpdateScoreText();
+	}
 
-            // Agent Component Check. Also important.
-            if (_agent == null)
-            {
-                Debug.LogError("Agent with 'agent' tag not found!");
-                return;
-            }
+	private void InitializeCanvas()
+	{
+		effectCanvas.renderMode = RenderMode.ScreenSpaceCamera;
+	}
 
-            _cameraAgent = _agent.transform.Find("AgentCamMid").GetComponent<Camera>();
+	private void LoadCamerasAndAgent()
+	{
+		screenshotCam = FindObjectOfType<ScreenshotCamera>();
+		cameras[0] = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+		cameras[1] = GameObject.FindGameObjectWithTag("agent").transform.Find("AgentCamMid").GetComponent<Camera>();
+		cameras[2] = GameObject.FindGameObjectWithTag("camBase").GetComponent<Camera>();
+		agent = GameObject.FindGameObjectWithTag("agent").GetComponent<TrainingAgent>();
+	}
 
-            // Camera Agent Check
-            if (_cameraAgent == null)
-            {
-                Debug.LogError("Camera 'AgentCamMid' not found in agent!");
-                return;
-            }
+	private void LoadConfigurationSettings()
+	{
+		arenasConfigurations = ArenasConfigurations.Instance;
+		canResetEpisode = arenasConfigurations?.canResetEpisode ?? true;
+		canChangePerspective = arenasConfigurations?.canChangePerspective ?? true;
+	}
 
-            camerasFetched = true;
+	private void InitializeCameras()
+	{
+		if (screenshotCam == null || agent == null || cameras[0] == null || cameras[1] == null || cameras[2] == null)
+		{
+			Debug.LogError("One or more essential components (Cameras, Agent, ScreenshotCam) were not found.");
+			return;
+		}
 
-            _cameraAbove.enabled = true;
-            _cameraAgent.enabled = false;
-            _cameraFollow.enabled = false;
+		activeCameraIndex = canChangePerspective ? 0 : 2; // If can't change perspectives, use the 3rd person camera
+		Debug.Log($"Initializing Cameras. Can Change Perspective: {canChangePerspective}, Active Camera Index: {activeCameraIndex}");
 
-            _cameras = new Dictionary<int, Camera>();
-            _cameras.Add(0, _cameraAbove);
-            _cameras.Add(1, _cameraAgent);
-            _cameras.Add(2, _cameraFollow);
+		SetActiveCamera(activeCameraIndex);
+		camerasInitialized = true;
+	}
 
-            effectCanvas.worldCamera = getActiveCam();
-            effectCanvas.planeDistance = choosePlaneDistance();
+	private void HandleInput()
+	{
+		if (canChangePerspective && Input.GetKeyDown(KeyCode.C))
+		{
+			CycleCamera();
+		}
+		if (canResetEpisode && Input.GetKeyDown(KeyCode.R))
+		{
+			agent.EndEpisode();
+		}
+		if (Input.GetKeyDown(KeyCode.Q))
+		{
+			Application.Quit();
+		}
+		if (Input.GetKeyDown(KeyCode.F9))
+		{
+			screenshotCam.Activate();
+		}
+	}
 
-            if (!_canChangePerspective)
-            {
-                _cameraAbove.enabled = false;
-                _cameraAgent.enabled = true;
-                _cameraFollow.enabled = false;
-                _numActive = 1;
+	private void UpdateScoreText()
+	{
+		if (agent != null && scoreText != null)
+		{
+			scoreText.text = $"Previous Reward: {agent.GetPreviousScore():0.000}\n" +
+							 $"Current Reward: {agent.GetCumulativeReward():0.000}";
+		}
+	}
 
-                effectCanvas.worldCamera = getActiveCam();
-                effectCanvas.planeDistance = choosePlaneDistance();
-            }
+	private void CycleCamera()
+	{
+		cameras[activeCameraIndex].enabled = false;
+		activeCameraIndex = (activeCameraIndex + 1) % cameras.Length;
+		SetActiveCamera(activeCameraIndex);
+	}
 
-            if (_cameras == null || _cameras.Count != 3)
-            {
-                Debug.LogError("Cameras dictionary not initialized properly!");
-                return;
-            }
-        }
+	private void SetActiveCamera(int index)
+	{
+		for (int i = 0; i < cameras.Length; i++)
+		{
+			cameras[i].enabled = (i == index);
+		}
+		effectCanvas.worldCamera = cameras[index];
+		effectCanvas.planeDistance = index == 1 ? 0.02f : 0.31f;
+	}
 
-        if (_canChangePerspective && Input.GetKeyDown(KeyCode.C))
-        {
-            UpdateCam();
-        }
-        if (_canResetEpisode && Input.GetKeyDown(KeyCode.R))
-        {
-            _agent.EndEpisode();
-        }
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            Application.Quit();
-        }
-        if (Input.GetKeyDown(KeyCode.F9))
-        {
-            _screenshotCam.Activate();
-        }
-
-        score.text =
-            "Previous Reward: "
-            + _agent.GetPreviousScore().ToString("0.000")
-            + "\n"
-            + "Current Reward: "
-            + _agent.GetCumulativeReward().ToString("0.000");
-    }
-
-    public Camera getActiveCam()
-    {
-        return _cameras[_numActive];
-    }
-
-    private float choosePlaneDistance()
-    {
-        return _numActive == 1 ? 0.02f : 0.31f;
-    }
-
-    void UpdateCam()
-    {
-        _cameras[_numActive].enabled = false;
-        _numActive = (_numActive + 1) % 3;
-        _cameras[_numActive].enabled = true;
-
-        effectCanvas.worldCamera = getActiveCam();
-        effectCanvas.planeDistance = choosePlaneDistance();
-    }
 }
