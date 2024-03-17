f0108bdb67a8ca4ceb57fbd40772b5fb47798703
alhasacademy96 <65875290+alhasacademy96@users.noreply.github.com>
2024-03-17 00:03:09 +0000

    cleaned up grounded.cs

--- Assets/Scripts/Grounded.cs
+++ Assets/Scripts/Grounded.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
 
+/// <summary>
+/// Represents the ground in the environment.
+/// </summary>
 public class Grounded : Prefab
 {
-    public float heightSpawn;
-    protected override float AdjustY(float yIn)
-    {
-        return 0;
-    }
+	protected override float AdjustY(float yIn)
+	{
+		return 0;
+	}
 }
