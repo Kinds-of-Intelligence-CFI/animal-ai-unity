11dfd590d4d7ba9c3f69e8d823186744e8d1381f
Ibrahim Alhas <65875290+alhasacademy96@users.noreply.github.com>
2024-03-18 22:16:37 +0000

    Update UIManager.cs

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -5,37 +5,36 @@ using System.IO;
 
 public class UIManager : MonoBehaviour
 {
-	public TMP_Text arenaText;
-	public TMP_Text totalObjectsText;
-	public AAI3EnvironmentManager environmentManager;
-	public TrainingArena trainingArena;
+    [Header("UI References")]
+    public TMP_Text arenaText;
+    public TMP_Text totalObjectsText;
+    public AAI3EnvironmentManager environmentManager;
 
-	void Awake()
-	{
-		AAI3EnvironmentManager.OnArenaChanged += UpdateArenaUI;
-	}
+    public TrainingArena trainingArena;
 
-	void OnDestroy()
-	{
-		AAI3EnvironmentManager.OnArenaChanged -= UpdateArenaUI;
-	}
+    void Awake()
+    {
+        AAI3EnvironmentManager.OnArenaChanged += UpdateArenaUI;
+    }
 
-	private void Start()
-	{
-		trainingArena = GameObject.FindObjectOfType<TrainingArena>();
-		Debug.Assert(trainingArena != null, "TrainingArena not found in the scene");
-		UpdateArenaUI(
-			environmentManager.GetCurrentArenaIndex(),
-			environmentManager.GetTotalArenas()
-		);
-	}
+    void OnDestroy()
+    {
+        AAI3EnvironmentManager.OnArenaChanged -= UpdateArenaUI;
+    }
 
-	private void UpdateArenaUI(int currentArenaIndex, int totalArenas)
-	{
-		// Update the arena information text
-		arenaText.text = $"Arena {currentArenaIndex + 1} of {totalArenas}";
-		// Fetch and display the total number of spawned objects from the TrainingArena
-		totalObjectsText.text = $"Total Objects: {trainingArena.Builder.GetTotalObjectsSpawned()}";
+    private void Start()
+    {
+        trainingArena = GameObject.FindObjectOfType<TrainingArena>();
+        Debug.Assert(trainingArena != null, "TrainingArena not found in the scene");
+        UpdateArenaUI(
+            environmentManager.GetCurrentArenaIndex(),
+            environmentManager.GetTotalArenas()
+        );
+    }
 
-	}
+    private void UpdateArenaUI(int currentArenaIndex, int totalArenas)
+    {
+        arenaText.text = $"Arena {currentArenaIndex + 1} of {totalArenas}";
+        totalObjectsText.text = $"Total Objects: {trainingArena.Builder.GetTotalObjectsSpawned()}";
+    }
 }
