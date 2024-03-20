bf686d0f8a5d35cd274afb2c4518f42f9ce51463
Ibrahim Alhas <65875290+alhasacademy96@users.noreply.github.com>
2024-03-20 13:43:11 +0000

    improved spawnerstockpiler.cs
    corrected and better formulated variable and method names,
    improved code flow,
    removed redundant comments.

--- Assets/Prefabs/Spawners/SpawnerStockpiler.cs
+++ Assets/Prefabs/Spawners/SpawnerStockpiler.cs
@@ -3,81 +3,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// SpawnerStockpiler is a GoalSpawner that can stockpile goals until there is space to materialize them.
+/// </summary>
 public class SpawnerStockpiler : GoalSpawner
 {
-    public override void SetInitialValue(float v)
-    {
-        base.SetInitialValue(Mathf.Max(Mathf.Min(v, 1f), 0.2f));
-    }
-
-    public override void SetFinalValue(float v)
-    {
-        base.SetFinalValue(Mathf.Max(Mathf.Min(v, 1f), 0.2f));
-    }
-
-    public bool stockpiling = true;
-    public int doorOpenDelay = -1; // -1 means "open indefinitely"
-
-    public override void SetDoorDelay(float v)
-    {
-        doorOpenDelay = (int)v;
-    }
-
-    public bool infiniteDoorOpens = false;
+    [Header("Stockpiling Settings")]
+    public bool isStockpiling = true;
+    public int doorOpenDelay = -1; // '-1' means "open indefinitely"
+    public bool isDoorOpenInfinite = false;
     public float timeUntilDoorOpens = 1.5f;
     public float timeUntilDoorCloses = 1.5f;
     public float minDoorOpenTime = 1.4f;
-    private Queue<BallGoal> waitingList;
-    private GameObject Door;
-    private int apparentSpawnCount;
 
-    public override void SetTimeBetweenDoorOpens(float v)
-    {
-        // Check if set to -1 (or < 0); this means "open indefinitely"
-        if (v < 0)
-        {
-            infiniteDoorOpens = false;
-            return;
-        }
-        // Then check is actually above minimum door open time
-        if (v < minDoorOpenTime)
-        {
-            Debug.Log(
-                "Invalid TimeBetweenDoorOpens (value "
-                    + v
-                    + " too small)."
-                    + "Clamping at minimum door-open time: "
-                    + minDoorOpenTime
-            );
-            v = minDoorOpenTime;
-        }
-
-        if (v < timeUntilDoorCloses)
-        {
-            timeUntilDoorCloses = v;
-        }
-        // 'Closed -> open' door change time is remainder
-        // ...(i.e. subtract 'open -> closed' door change time)
-        timeUntilDoorOpens = v - timeUntilDoorCloses;
-    }
+    private GameObject doorObject;
+    private Queue<BallGoal> waitingGoals = new Queue<BallGoal>();
 
     public override void Awake()
     {
         base.Awake();
-        if (stockpiling)
+        InitializeVariables();
+    }
+
+    private void InitializeVariables()
+    {
+        if (isStockpiling)
         {
-            Door = transform.GetChild(1).gameObject;
-            if (!Door.name.ToLower().Contains("door"))
-                throw new Exception("WARNING: a stockpiling GoalSpawner has not found its Door.");
+            doorObject = transform.GetChild(1).gameObject;
+            if (!doorObject.name.ToLower().Contains("door"))
+                throw new Exception("WARNING: A stockpiling GoalSpawner has not found its Door.");
+
+            StartCoroutine(ManageDoor());
         }
 
-        waitingList = new Queue<BallGoal>();
-        if (stockpiling)
-        {
-            StartCoroutine(manageDoor());
-        }
-
-        if (infiniteDoorOpens)
+        if (isDoorOpenInfinite)
         {
             if (timeUntilDoorCloses < minDoorOpenTime)
             {
@@ -89,7 +48,7 @@ public class SpawnerStockpiler : GoalSpawner
             if (timeUntilDoorOpens < 0)
             {
                 timeUntilDoorOpens = 0;
-                Debug.Log("WARNING: negative TimeUntilDoorOpens given. Clamping to 0...");
+                Debug.Log("WARNING: Negative TimeUntilDoorOpens given. Clamping to 0...");
             }
         }
         else
@@ -101,15 +60,38 @@ public class SpawnerStockpiler : GoalSpawner
         canRandomizeColor = true;
     }
 
-    private void FixedUpdate()
+    // Door control coroutine
+    private IEnumerator ManageDoor(bool includeInitDelay = true, bool isDoorOpening = true)
     {
-        if (waitingList.Count > 0 && freeToMaterialise(ripenedSpawnSize))
+        if (includeInitDelay)
+            yield return new WaitForSeconds(Mathf.Max(doorOpenDelay, 0));
+
+        float deltaTime = 0f;
+        float newSize;
+        while (deltaTime < 1)
         {
-            print("waitingList.Count: " + waitingList.Count);
-            BallGoal newGoal = waitingList.Dequeue();
-            print("post-Dequeue() waitingList.Count: " + waitingList.Count);
-            print("materialising newGoal: " + newGoal.name);
-            immaterialStorageChange(newGoal, true);
+            newSize = base.Interpolate(
+                0,
+                1,
+                deltaTime,
+                isDoorOpening ? 1 : 0,
+                isDoorOpening ? 0 : 1
+            );
+            doorObject.transform.localScale = new Vector3(
+                doorObject.transform.localScale.x,
+                newSize,
+                doorObject.transform.localScale.z
+            );
+            deltaTime += Time.fixedDeltaTime;
+            yield return new WaitForSeconds(Time.fixedDeltaTime);
+        }
+
+        if (isDoorOpenInfinite)
+        {
+            yield return new WaitForSeconds(
+                isDoorOpening ? timeUntilDoorCloses : timeUntilDoorOpens
+            );
+            StartCoroutine(ManageDoor(false, !isDoorOpening));
         }
     }
 
@@ -118,72 +100,77 @@ public class SpawnerStockpiler : GoalSpawner
         BallGoal newGoal = base.SpawnNewGoal(listID);
         newGoal.name += spawnCount + 1;
 
-        if (!freeToMaterialise(ripenedSpawnSize, newGoal))
+        if (!FreeToMaterialize(ripenedSpawnSize, newGoal))
         {
-            immaterialStorageChange(newGoal, false);
-            waitingList.Enqueue(newGoal);
+            ChangeImmaterialStorage(newGoal, false);
+            waitingGoals.Enqueue(newGoal);
         }
 
         return newGoal;
     }
 
-    private bool freeToMaterialise(float r, BallGoal g = null)
+    private void FixedUpdate()
+    {
+        if (waitingGoals.Count > 0 && FreeToMaterialize(ripenedSpawnSize))
+        {
+            BallGoal newGoal = waitingGoals.Dequeue();
+            ChangeImmaterialStorage(newGoal, true);
+        }
+    }
+
+    // Check if there is space to materialize a new goal
+    private bool FreeToMaterialize(float radius, BallGoal goal = null)
     {
         Collider[] sphereCheck = Physics.OverlapSphere(
             transform.position + defaultSpawnPosition,
-            r * 0.4f
+            radius * 0.4f
         );
         foreach (Collider col in sphereCheck)
         {
-            if (col.gameObject.tag == spawnObjects[0].tag && (g == null || col.gameObject != g))
-            {
+            if (
+                col.gameObject.tag == spawnObjects[0].tag
+                && (goal == null || col.gameObject != goal)
+            )
                 return false;
-            }
         }
         return true;
     }
 
-    // Physically materialises a spawned goal stuck on the waiting list
-    // ...or dematerialises if spawned goal needs to wait
-    private void immaterialStorageChange(BallGoal goal, bool materialising)
+    // Physically materialize or dematerialize a spawned goal
+    private void ChangeImmaterialStorage(BallGoal goal, bool isMaterializing)
     {
-        goal.GetComponent<MeshRenderer>().enabled = materialising;
-        goal.GetComponent<SphereCollider>().enabled = materialising;
+        goal.GetComponent<MeshRenderer>().enabled = isMaterializing;
+        goal.GetComponent<SphereCollider>().enabled = isMaterializing;
         Rigidbody rb = goal.GetComponent<Rigidbody>();
-        rb.useGravity = materialising;
-        rb.isKinematic = !materialising;
-        rb.constraints = materialising
+        rb.useGravity = isMaterializing;
+        rb.isKinematic = !isMaterializing;
+        rb.constraints = isMaterializing
             ? RigidbodyConstraints.None
             : RigidbodyConstraints.FreezePosition;
     }
 
-    private IEnumerator manageDoor(bool includeInitDelay = true, bool doorOpening = true)
+    public override void SetTimeBetweenDoorOpens(float value)
     {
-        if (includeInitDelay)
+        if (value < 0)
         {
-            yield return new WaitForSeconds(Math.Max(doorOpenDelay, 0));
+            isDoorOpenInfinite = false;
+            return;
         }
 
-        float dt = 0f;
-        float newSize;
-        while (dt < 1)
+        if (value < minDoorOpenTime)
         {
-            newSize = base.interpolate(0, 1, dt, doorOpening ? 1 : 0, doorOpening ? 0 : 1);
-            Door.transform.localScale = new Vector3(
-                Door.transform.localScale.x,
-                newSize,
-                Door.transform.localScale.z
+            Debug.Log(
+                "Invalid TimeBetweenDoorOpens (value "
+                    + value
+                    + " too small). Clamping at minimum door-open time: "
+                    + minDoorOpenTime
             );
-            dt += Time.fixedDeltaTime;
-            yield return new WaitForSeconds(Time.fixedDeltaTime);
+            value = minDoorOpenTime;
         }
 
-        if (infiniteDoorOpens)
-        {
-            yield return new WaitForSeconds(doorOpening ? timeUntilDoorCloses : timeUntilDoorOpens);
-            StartCoroutine(manageDoor(false, !doorOpening));
-        }
+        if (value < timeUntilDoorCloses)
+            timeUntilDoorCloses = value;
 
-        yield return null;
+        timeUntilDoorOpens = value - timeUntilDoorCloses;
     }
 }
