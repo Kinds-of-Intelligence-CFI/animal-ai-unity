98dc489095e117226546af8596c2d3494480cbab
alhasacademy96 <65875290+alhasacademy96@users.noreply.github.com>
2024-03-26 19:50:26 +0000

    Update Grounded.cs

--- Assets/Scripts/Grounded.cs
+++ Assets/Scripts/Grounded.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
 /// <summary>
-/// Represents the ground in the environment.
+/// Represents the ground in the environment. This class is used to adjust the y position of the ground.
 /// </summary>
 public class Grounded : Prefab
 {
