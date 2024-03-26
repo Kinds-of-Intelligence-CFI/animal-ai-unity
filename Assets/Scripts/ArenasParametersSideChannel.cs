cba3277794bbb053f81f8de5099d9331d87da2e2
alhasacademy96 <65875290+alhasacademy96@users.noreply.github.com>
2024-03-26 19:49:19 +0000

    added comments to methods for explanation

--- Assets/Scripts/ArenasParametersSideChannel.cs
+++ Assets/Scripts/ArenasParametersSideChannel.cs
@@ -7,11 +7,17 @@ using ArenasParameters;
 /// </summary>
 public class ArenasParametersSideChannel : SideChannel
 {
+	/// <summary>
+	/// Initializes a new instance of the ArenasParametersSideChannel class.
+	/// </summary>
 	public ArenasParametersSideChannel()
 	{
 		ChannelId = new Guid("9c36c837-cad5-498a-b675-bc19c9370072");
 	}
 
+	/// <summary>
+	/// This method is called when a message is received from the Unity.
+	/// </summary>
 	protected override void OnMessageReceived(IncomingMessage msg)
 	{
 		ArenasParametersEventArgs args = new ArenasParametersEventArgs();
@@ -19,6 +25,9 @@ public class ArenasParametersSideChannel : SideChannel
 		OnArenasParametersReceived(args);
 	}
 
+	/// <summary>
+	/// This method is called when the arenas parameters are received.
+	/// </summary>
 	protected virtual void OnArenasParametersReceived(
 		ArenasParametersEventArgs arenasParametersEvent
 	)
@@ -29,7 +38,10 @@ public class ArenasParametersSideChannel : SideChannel
 			handler(this, arenasParametersEvent);
 		}
 	}
-
+	
+	/// <summary>
+	/// This event is triggered when new arenas parameters are received.
+	/// </summary>
 	public EventHandler<ArenasParametersEventArgs> NewArenasParametersReceived;
 
 }
