1d83fbd35a54615c51bfe348106a57c4ce6a8026
Ibrahim Alhas <65875290+alhasacademy96@users.noreply.github.com>
2024-04-08 23:23:16 +0100

    maintained progressbar.cs as was forgotten from PR#32

--- Assets/ProgressBar/ProgressBar.cs
+++ Assets/ProgressBar/ProgressBar.cs
@@ -9,7 +9,7 @@ using UnityEngine.UI;
 [ExecuteInEditMode]
 public class ProgressBar : MonoBehaviour
 {
-    [Header("Title Setting")]
+    [Header("Title Text Setting")]
     public string Title;
     public Color TitleColor;
     public TMP_FontAsset TitleFont;
@@ -26,21 +26,19 @@ public class ProgressBar : MonoBehaviour
     public int StartHealth = 0;
     public int PassMark = 0;
 
-    private Image _bar,
-        _barBackground;
-    private RectTransform _passMarker;
-    private float _nextPlay;
-    private TMP_Text _txtTitle;
-    private TrainingAgent _agent;
-
     [Header("Pass Mark Settings")]
     private float _pass_mark_proportion;
     private float _min_pass_color_pivot;
     private float _pass_max_color_pivot;
 
+    private Image _bar,
+        _barBackground;
+    private RectTransform _passMarker;
+    private TMP_Text _txtTitle;
+    private TrainingAgent _agent;
+
     private int _barSize;
     private float _barValue;
-
     public float BarValue
     {
         get { return _barValue; }
@@ -58,14 +56,18 @@ public class ProgressBar : MonoBehaviour
         _txtTitle = transform.Find("Text").GetComponent<TMP_Text>();
         _barBackground = transform.Find("BarBackground").GetComponent<Image>();
         _passMarker = transform.Find("PassMarker").GetComponent<RectTransform>();
+
+        InitializeProgressBar();
     }
 
-    private void Start()
+    private void InitializeProgressBar()
     {
         _txtTitle.text = Title;
         _txtTitle.color = TitleColor;
         _txtTitle.font = TitleFont;
         _txtTitle.fontSize = TitleFontSize;
+        _barBackground.color = BarBackGroundColor;
+        _barBackground.sprite = BarBackGroundSprite;
 
         _barSize = MaxHealth - MinHealth;
         StartHealth = Clamp(StartHealth);
@@ -78,69 +80,7 @@ public class ProgressBar : MonoBehaviour
         _bar.fillAmount = HealthProportion(StartHealth);
 
         UpdateColor(_bar.fillAmount);
-        _barBackground.color = BarBackGroundColor;
-        _barBackground.sprite = BarBackGroundSprite;
-
         UpdateValue(StartHealth);
-    }
-
-    private float HealthProportion(float x)
-    {
-        return ((x - MinHealth) / _barSize);
-    }
-
-    private int Clamp(int h)
-    {
-        return Mathf.Clamp(h, MinHealth, MaxHealth);
-    }
-
-    private float Clamp(float h)
-    {
-        return Mathf.Clamp(h, MinHealth, MaxHealth);
-    }
-
-    void UpdateValue(float val)
-    {
-        if (val != Mathf.Clamp(val, MinHealth, MaxHealth))
-        {
-            Debug.Log("intended progress bar value out of bounds! Clamping . . .");
-            val = Mathf.Clamp(val, MinHealth, MaxHealth);
-        }
-        float health_proportion = HealthProportion(val);
-        _bar.fillAmount = health_proportion;
-        _txtTitle.text = Title + ": " + val.ToString("F1");
-
-        UpdateColor(_bar.fillAmount);
-    }
-
-    private void UpdateColor(float fill)
-    {
-        if (fill != Mathf.Clamp(fill, 0, 1))
-        {
-            Debug.Log("UpdateColor passed a bad fill proportion! Clamping . . .");
-            fill = Mathf.Clamp(fill, 0, 1);
-        }
-        if (fill < _pass_mark_proportion)
-        {
-            fill = (fill / _pass_mark_proportion);
-            _bar.color =
-                fill * BarNormalColor
-                + (1 - fill) * BarEmptyColor
-                + (0.5f - Mathf.Abs(fill - 0.5f)) * Color.white;
-        }
-        else
-        {
-            fill = ((fill - _pass_mark_proportion) / (1 - _pass_mark_proportion));
-            _bar.color =
-                fill * BarFullColor
-                + (1 - fill) * BarNormalColor
-                + (0.5f - Mathf.Abs(fill - 0.5f)) * Color.white * 0.5f;
-        }
-    }
-
-    public void AssignAgent(TrainingAgent training_agent)
-    {
-        _agent = training_agent;
     }
 
     private void FixedUpdate()
@@ -163,7 +103,74 @@ public class ProgressBar : MonoBehaviour
         }
         else
         {
-            Debug.Log("No agent assigned to progress bar!");
+            Debug.Log("Agent properties null or not assigned! Cannot update progress bar!");
+        }
+    }
+
+    private float HealthProportion(float x)
+    {
+        return ((x - MinHealth) / _barSize);
+    }
+
+    private int Clamp(int h)
+    {
+        return Mathf.Clamp(h, MinHealth, MaxHealth);
+    }
+
+    private float Clamp(float h)
+    {
+        return Mathf.Clamp(h, MinHealth, MaxHealth);
+    }
+
+    void UpdateValue(float val)
+    {
+        if (val != Mathf.Clamp(val, MinHealth, MaxHealth))
+        {
+            Debug.Log("Progress bar value out of bounds! Clamping...");
+            val = Mathf.Clamp(val, MinHealth, MaxHealth);
+        }
+        float health_proportion = HealthProportion(val);
+        _bar.fillAmount = health_proportion;
+        _txtTitle.text = Title + ": " + val.ToString("F1");
+
+        UpdateColor(_bar.fillAmount);
+    }
+
+    private void UpdateColor(float fill)
+    {
+        if (fill != Mathf.Clamp(fill, 0, 1))
+        {
+            Debug.Log("UpdateColor() in ProgressBar.cs passed a bad fill proportion! Clamping...");
+            fill = Mathf.Clamp(fill, 0, 1);
+        }
+        if (fill < _pass_mark_proportion)
+        {
+            fill = (fill / _pass_mark_proportion);
+            _bar.color =
+                fill * BarNormalColor
+                + (1 - fill) * BarEmptyColor
+                + (0.5f - Mathf.Abs(fill - 0.5f)) * Color.white;
+        }
+        else
+        {
+            fill = ((fill - _pass_mark_proportion) / (1 - _pass_mark_proportion));
+            _bar.color =
+                fill * BarFullColor
+                + (1 - fill) * BarNormalColor
+                + (0.5f - Mathf.Abs(fill - 0.5f)) * Color.white * 0.5f;
+        }
+    }
+
+    public void AssignAgent(TrainingAgent training_agent)
+    {
+        if (training_agent == null)
+        {
+            Debug.Log("Agent is null or no agent found for assignment!");
+            return;
+        }
+        else
+        {
+            _agent = training_agent;
         }
     }
 }
