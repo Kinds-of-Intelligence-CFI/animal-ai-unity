ae1280f0d410eea8a8ec7a8b3de12b185e9198a9
alhasacademy96 <65875290+alhasacademy96@users.noreply.github.com>
2024-06-03 21:30:43 +0100

    Update ArenasParametersSideChannel.cs

--- Assets/Scripts/ArenasParametersSideChannel.cs
+++ Assets/Scripts/ArenasParametersSideChannel.cs
@@ -8,62 +8,52 @@ using UnityEngine;
 /// </summary>
 public class ArenasParametersSideChannel : SideChannel
 {
-    public string CurrentYamlFileName { get; private set; } // Store the current YAML file name
+	public string CurrentYamlFileName { get; private set; } // Store the current YAML file name
 
-    /// <summary>
-    /// Initializes a new instance of the ArenasParametersSideChannel class.
-    /// </summary>
-    public ArenasParametersSideChannel()
-    {
-        ChannelId = new Guid("9c36c837-cad5-498a-b675-bc19c9370072");
-    }
+	/// <summary>
+	/// Initializes a new instance of the ArenasParametersSideChannel class.
+	/// </summary>
+	public ArenasParametersSideChannel()
+	{
+		ChannelId = new Guid("9c36c837-cad5-498a-b675-bc19c9370072");
+	}
 
-    /// <summary>
-    /// This method is called when a message is received from the Unity.
-    /// </summary>
-    protected override void OnMessageReceived(IncomingMessage msg)
-    {
-        string fileName = msg.ReadString();
-        byte[] yamlData = msg.GetRawBytes();
+	/// <summary>
+	/// This method is called when a message is received from the Unity.
+	/// </summary>
+	protected override void OnMessageReceived(IncomingMessage msg)
+	{
+		string fileName = msg.ReadString();
+		byte[] yamlData = msg.GetRawBytes();
 
-        Debug.Log($"Received YAML file name: {fileName}");
-        CurrentYamlFileName = fileName;
+		Debug.Log($"Received YAML file name: {fileName}");
+		CurrentYamlFileName = fileName;
 
-        // Create the event args including the file name and the YAML data
-        ArenasParametersEventArgs args = new ArenasParametersEventArgs
-        {
-            arenas_yaml = yamlData,
-            yamlFileName = fileName
-        };
-        OnArenasParametersReceived(args);
-		UpdateTrainingAgentsWithYamlFileName(fileName);
-    }
+		// Create the event args including the file name and the YAML data
+		ArenasParametersEventArgs args = new ArenasParametersEventArgs
+		{
+			arenas_yaml = yamlData,
+			yamlFileName = fileName
+		};
+		OnArenasParametersReceived(args);
+	}
 
-    /// <summary>
-    /// This method is called when the arenas parameters are received.
-    /// </summary>
-    protected virtual void OnArenasParametersReceived(
-        ArenasParametersEventArgs arenasParametersEvent
-    )
-    {
-        EventHandler<ArenasParametersEventArgs> handler = NewArenasParametersReceived;
-        if (handler != null)
-        {
-            handler(this, arenasParametersEvent);
-        }
-    }
+	/// <summary>
+	/// This method is called when the arenas parameters are received.
+	/// </summary>
+	protected virtual void OnArenasParametersReceived(
+		ArenasParametersEventArgs arenasParametersEvent
+	)
+	{
+		EventHandler<ArenasParametersEventArgs> handler = NewArenasParametersReceived;
+		if (handler != null)
+		{
+			handler(this, arenasParametersEvent);
+		}
+	}
 
-    private void UpdateTrainingAgentsWithYamlFileName(string fileName)
-    {
-        TrainingAgent[] agents = GameObject.FindObjectsOfType<TrainingAgent>();
-        foreach (var agent in agents)
-        {
-            agent.SetYamlFileName(fileName);
-        }
-    }
-
-    /// <summary>
-    /// This event is triggered when new arenas parameters are received.
-    /// </summary>
-    public EventHandler<ArenasParametersEventArgs> NewArenasParametersReceived;
+	/// <summary>
+	/// This event is triggered when new arenas parameters are received.
+	/// </summary>
+	public EventHandler<ArenasParametersEventArgs> NewArenasParametersReceived;
 }
