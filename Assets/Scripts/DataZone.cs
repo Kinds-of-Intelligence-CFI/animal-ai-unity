02102043c075a0ea91da504f43aa437f274cdf3e
Ibrahim Alhas <65875290+alhasacademy96@users.noreply.github.com>
2024-06-23 21:40:22 +0100

    implemented basic datazone that logs to csv file

--- Assets/Scripts/DataZone.cs
+++ Assets/Scripts/DataZone.cs
@@ -1,18 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// A zone that triggers an event when an agent enters it. It's primary use is to trigger data collection.
+/// </summary>
 public class DataZone : Prefab
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // TODO: Add logic to differentiate between different datazones
+    // TODO: Implement parameter to specify zone names/sections in yaml file (under the datazones Item)
+    public delegate void InDataZoneHandler(GameObject zone);
+    public static event InDataZoneHandler OnInDataZone;
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision other)
     {
-        
+        if (other.gameObject.CompareTag("agent"))
+        {
+            OnInDataZone?.Invoke(gameObject);
+        }
     }
 }
