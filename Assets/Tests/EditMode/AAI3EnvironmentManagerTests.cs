787b7a59a566a6a2ae03ce3085806e4dd6ede5a2
Ibrahim Alhas <65875290+alhasacademy96@users.noreply.github.com>
2024-08-15 18:27:22 +0100

    added the rest of the test units after analysis

--- Assets/Tests/EditMode/AAI3EnvironmentManagerTests.cs
+++ Assets/Tests/EditMode/AAI3EnvironmentManagerTests.cs
@@ -7,6 +7,7 @@ using Unity.MLAgents.Sensors;
 using Unity.MLAgents.SideChannels;
 using Unity.MLAgents.Policies;
 using ArenasParameters;
+using YamlDotNet.Serialization;
 
 /// <summary>
 /// Tests for the AAI3EnvironmentManager class.
@@ -43,6 +44,59 @@ public class AAI3EnvironmentManagerTests
         _environmentManager.playerControls = new GameObject();
 
         _environmentManager._arenasConfigurations = new ArenasConfigurations();
+    }
+
+    [Test]
+    public void AAI3EnvironmentManager_RetrieveEnvironmentParameters_HandlesInvalidArgsGracefully()
+    {
+        string[] args = { "--invalidArgument", "someValue" };
+        var parameters = _environmentManager.RetrieveEnvironmentParameters(args);
+
+        Assert.IsFalse(parameters.ContainsKey("invalidArgument"));
+    }
+
+    [Test]
+    public void AAI3EnvironmentManager_GetTotalArenas_ReturnsCorrectCount()
+    {
+        _environmentManager.AddConfiguration(0, new ArenaConfiguration());
+        _environmentManager.AddConfiguration(1, new ArenaConfiguration());
+
+        int totalArenas = _environmentManager.GetTotalArenas();
+        Assert.AreEqual(2, totalArenas);
+    }
+
+    [Test]
+    public void AAI3EnvironmentManager_LoadYAMLFileInEditor_LoadsValidYAML()
+    {
+        _environmentManager.configFile = "test_configs/decoy-file-test"; /* File should exist and be in resources folder */
+        _environmentManager.LoadYAMLFileInEditor();
+
+        Assert.IsTrue(_environmentManager.GetTotalArenas() > 0);
+    }
+
+    [Test]
+    public void AAI3EnvironmentManager_InstantiateArenas_CreatesMultipleArenas()
+    {
+        _environmentManager.InstantiateArenas();
+
+        Assert.IsNotNull(_environmentManager.arena);
+        Assert.IsInstanceOf<TrainingArena>(_environmentManager.arena.GetComponent<TrainingArena>());
+    }
+
+    [Test]
+    public void AAI3EnvironmentManager_OnDestroy_UnregistersSideChannel()
+    {
+        _environmentManager.InitialiseSideChannel();
+        _environmentManager.OnDestroy();
+
+        Assert.Pass("Side channel unregistered without exceptions.");
+    }
+
+    [Test]
+    public void AAI3EnvironmentManager_InitialiseSideChannel_CorrectlyInitializesSideChannel()
+    {
+        _environmentManager.InitialiseSideChannel();
+        Assert.IsNotNull(_environmentManager.ArenasParametersSideChannel);
     }
 
     [Test]
@@ -146,5 +200,21 @@ public class AAI3EnvironmentManagerTests
 
         AAI3EnvironmentManager.OnArenaChanged -= (currentArenaIndex, totalArenas) =>
             eventTriggered = true;
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        if (_environmentManager != null && _environmentManager.ArenasParametersSideChannel != null)
+        {
+            SideChannelManager.UnregisterSideChannel(
+                _environmentManager.ArenasParametersSideChannel
+            );
+        }
+
+        GameObject.DestroyImmediate(_gameObject);
+        GameObject.DestroyImmediate(_trainingArena.gameObject);
+        GameObject.DestroyImmediate(_agent.gameObject);
+        GameObject.DestroyImmediate(_canvas.gameObject);
     }
 }
