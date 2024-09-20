2cc18ce8a17cd3fda336474739c38dab711ce034
Ibrahim Alhas <alhasacademy@gmail.com>
2024-09-20 20:44:47 +0100

    modified prefab.cs and goal.cs to accept yaml colors as overrides
    The default colors are applied if no specified colors are present for the objects that support this new feature. Also improved performance by only creating a new material instance for the object if the color is to be changed.
    
    Added a new method specifically for handling render-related color specifications. This is needed as the goals (rewards such as GoodGoal) have an emissive property, which must match the basecolor. If they dont, there will be a blend between default and specified yaml colors.
    
    Added checks for HollowBox (special case here) to apply the color changes to it's child objects - otherwise the color changes would not apply.

--- Assets/Scripts/Rewards/Goal.cs
+++ Assets/Scripts/Rewards/Goal.cs
@@ -16,6 +16,54 @@ public class Goal : Prefab
         canRandomizeColor = false;
     }
 
+    protected override void ApplyColorToRenderer(Renderer renderer, Color color, bool colorSpecified)
+    {
+        if (!colorSpecified)
+        {
+            return;
+        }
+
+        /* Create a new material to apply color changes. This is unique for each instance */
+        Material originalMaterial = renderer.material;
+        renderer.material = new Material(originalMaterial);
+
+        if (renderer.material.HasProperty("_BaseColor"))
+        {
+            renderer.material.SetColor("_BaseColor", color);
+        }
+        else
+        {
+            renderer.material.color = color;
+        }
+
+        if (colorSpecified || canRandomizeColor)
+        {
+            /* Apply emissive color if available */
+            if (renderer.material.HasProperty("_EmissionColor"))
+            {
+                renderer.material.SetColor("_EmissionColor", color);
+                renderer.material.EnableKeyword("_EMISSION");
+            }
+        }
+        else
+        {
+            /* Copy over the emissive color from the original material */
+            if (renderer.material.HasProperty("_EmissionColor"))
+            {
+                Color originalEmissionColor = originalMaterial.GetColor("_EmissionColor");
+                renderer.material.SetColor("_EmissionColor", originalEmissionColor);
+                if (originalMaterial.IsKeywordEnabled("_EMISSION"))
+                {
+                    renderer.material.EnableKeyword("_EMISSION");
+                }
+                else
+                {
+                    renderer.material.DisableKeyword("_EMISSION");
+                }
+            }
+        }
+    }
+
     public virtual void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("agent"))
