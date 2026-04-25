6d40a5e5e8eb065dc99636aa5b398df1a0eee3ae
Ben Slater <bas58@cam.ac.uk>
2026-04-25 10:53:40 +0100

    Update WelGLConstants to prod resources
    These were switched to dev during the development of the lifecycle state lambda, so switching them back

--- Assets/Scripts/WebGLConstants.cs
+++ Assets/Scripts/WebGLConstants.cs
@@ -3,10 +3,10 @@
 /// </summary>
 public static class WebGLConstants
 {
-    public const string S3_BUCKET = "https://test-experiment-data-storage.s3.eu-north-1.amazonaws.com";
-    public const string CSV_UPLOAD_LAMBDA_ENDPOINT = "https://mjlo2ftcn3.execute-api.eu-north-1.amazonaws.com/Prod/telemetry";
+    public const string S3_BUCKET = "https://animal-ai-website-experiment-data-storage.s3.eu-north-1.amazonaws.com";
+    public const string CSV_UPLOAD_LAMBDA_ENDPOINT = "https://x314h4yzq5.execute-api.eu-north-1.amazonaws.com/Prod/telemetry";
 
-    public const string GET_USER_LIFECYCLE_STATE_LAMBDA_ENDPOINT = "https://ijxyyr92x3.execute-api.eu-north-1.amazonaws.com/test/telemetry";
+    public const string GET_USER_LIFECYCLE_STATE_LAMBDA_ENDPOINT = "https://ovqx9h40zg.execute-api.eu-north-1.amazonaws.com/test/telemetry";
 
     public const string TUTORIAL_CONFIG_FILENAME = "tutorial_config.yaml";
     public const string EXPERIMENT_CONFIG_FILENAME = "config.yaml";
